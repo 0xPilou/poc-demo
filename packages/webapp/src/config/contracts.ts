1763f0979dbe611c9d7e0eacb6235df71d00b1c8
Pilou <76021631+0xPilou@users.noreply.github.com>
2025-03-05 10:48:19 +0100

    feat: Update contract addresses and expand ERC20 ABI

--- packages/webapp/src/config/contracts.ts
+++ packages/webapp/src/config/contracts.ts
@@ -1,6 +1,7 @@
-export const CONTRACT_ADDRESS = "0x29754F3d807EC91066cF2693c92922E036E27E08";
+export const DEMO_GDA_ADDRESS = "0x56849c1c5dfbfa0620215171d1bef945e22db551";
+export const SUPERTOKEN_ADDRESS = "0xb75F6aEf9F3BcE91856946bcB15B875ddEc68c2D";
 
-export const CONTRACT_ABI = [
+export const DEMO_GDA_ABI = [
   {
     type: "constructor",
     inputs: [{ name: "_superToken", type: "address", internalType: "address" }],
@@ -20,11 +21,6 @@ export const CONTRACT_ABI = [
     type: "function",
     name: "createPool",
     inputs: [
-      {
-        name: "_superToken",
-        type: "address",
-        internalType: "contract ISuperToken",
-      },
       { name: "_poolName", type: "string", internalType: "string" },
       { name: "_poolSymbol", type: "string", internalType: "string" },
     ],
@@ -111,6 +107,40 @@ export const ERC20_ABI = [
     inputs: [],
     name: "symbol",
     outputs: [{ name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "spender", type: "address", internalType: "address" },
+      { name: "amount", type: "uint256", internalType: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ name: "", type: "bool", internalType: "bool" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ name: "account", type: "address", internalType: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { name: "owner", type: "address", internalType: "address" },
+      { name: "spender", type: "address", internalType: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8", internalType: "uint8" }],
     stateMutability: "view",
     type: "function",
   },
